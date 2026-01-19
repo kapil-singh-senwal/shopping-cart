e28039a8c60ce4db280262b1c1394599f9b27196
kapil-singh-senwal <kapilsinghsenwal@gmail.com>
2026-01-19 20:48:06 +0530

    removing browser router, to solve the blank page issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
+  <HashRouter>
   <Provider store= {store}>
-  <BrowserRouter>
+  {/* <BrowserRouter> */}
     <App />
-  </BrowserRouter>
+  {/* </BrowserRouter> */}
   </Provider>
+  </HashRouter>  
 );
